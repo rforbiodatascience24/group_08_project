b2623145364077a28790a583fc49045c14ddac4d
ceciliemoellerjensen <cecilie.moeller.jensen@gmail.com>
2024-11-26 11:32:39 +0100

    Delete another unused file in func doc

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -20,28 +20,6 @@ clean_data <- function(dataframe) {
 }
 
 
-# Filtering mean for no outliers
-calculate_filtered_mean <- function(data) {
-  # 1. Calculate quartiles and IQR
-  q1 <- quantile(data, 0.25) # First quartile (Q1)
-  q3 <- quantile(data, 0.75) # Third quartile (Q3)
-  iqr <- q3 - q1             # Interquartile range (IQR)
-  
-  # 2. Define bounds for outliers
-  lower_bound <- q1 - 1.5 * iqr
-  upper_bound <- q3 + 1.5 * iqr
-  
-  # 3. Filter out outliers
-  filtered_data <- data[data >= lower_bound & data <= upper_bound]
-  
-  # 4. Calculate the mean of the filtered data
-  mean_filtered <- round(mean(filtered_data, na.rm = TRUE), 2)
-  
-  # 5. Return results as a list
-  return(mean_filtered)
-}
-
-
 # Function to replace outliers with NA for columns starting with a specific string and add bounds
 replace_outliers_with_na <- function(data, prefix) {
   # Identify columns with the specified prefix
